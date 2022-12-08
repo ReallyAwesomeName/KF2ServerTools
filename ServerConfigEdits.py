01b2a2fc9c3e45b736cb1c01dd1ee3d50e3566d2
Jason <jg832@njit.edu>
2022-12-08 09:57:50 -0500

    added license notice

--- ServerConfigEdits.py
+++ ServerConfigEdits.py
@@ -1,5 +1,26 @@
-# Fully automate KF2 server map update process
 # https://github.com/ReallyAwesomeName/KF2ServerTools
+	
+    # KF2Server Tools - CLI Tools for managing a Dedicated Killing Floor 2 Server
+    # Copyright (C) 2022  Rin
+	
+	# This file is a part of KF2ServerTools
+
+    # KF2ServerTools is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # KF2ServerTools is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+# Discord: Rin#0304
+
+# ServerConfigEdits.py
 
 
 import argparse
